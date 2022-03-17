35dac377a055b656ff67005822fc358d4d913cf8
Stepan Subrt <stepansubrt@icloud.com>
2022-03-17 14:44:40 +0100

    build: rendering projects component + section container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Introduction from './components/Introduction/Introduction'
+import Projects from './components/Projects/Projects'
 
 const App = () => {
   return (
     <>
       <Introduction />
+      <Projects />
     </>
   )
 }
