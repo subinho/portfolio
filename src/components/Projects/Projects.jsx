28b4b20ce4cfa72577621cc8f0b0a191efd87284
Stepan Subrt <stepansubrt@icloud.com>
2022-03-17 14:51:20 +0100

    build: projects component + necessary files

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Projects.css'
+import data from './data'
+
+const Projects = () => {
+  return (
+      <section className='projects padd_page'>
+        <div className='section_container'>
+            <h2>Projects</h2>
+            <div className='projects_container'>
+                {
+                    data.map(({image, title, github, live}) => (
+                        <div className='project'>
+                            <h3>{title}</h3>
+                            <div className='project_container--image'>
+                                <img src={image} alt='' className='project--image'/>
+                            </div>
+                            <div className='project--links'>
+                                    <a href={live} target='_blank'>Live</a>
+                                    <a href={github} target='_blank'>Github</a>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+      </section>
+  )
+}
+
+export default Projects
---
+++ src/components/Projects/data.js
@@ -0,0 +1,40 @@
+import img from '../../images/coming-soon.jpeg'
+
+export default [
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    },
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    },
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    },
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    },
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    },
+    {
+        image: img,
+        title: 'Quiz App',
+        github: '#',
+        live: '#'
+    }
+]
