7f2a49c0ab1ec6fcec014de39f66de9c3f331073
Stepan Subrt <stepansubrt@icloud.com>
2022-04-01 00:48:35 +0200

    change: list to div, deleted unnecessary class

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,27 +5,27 @@ import { AiOutlineMail, AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
 const Footer = () => {
   return (
       <section className="footer">
-          <div className="content_container footer_container">
+          <div className="content_container">
 
-            <div className="footer--me">
-                <h3>Štěpán Šubrt</h3>
-                <p>WEB DEVELOPER</p>
+              <div className="footer--me">
+                  <h3>Štěpán Šubrt</h3>
+                  <p>WEB DEVELOPER</p>
+              </div>
+              
+              <ul className="footer--nav">
+                  <li><a href='#'>HOME</a></li>
+                  <li><a href='#'>PROJECTS</a></li>
+                  <li><a href='#'>ABOUT</a></li>
+                  <li><a href='#'>CONTACT</a></li>
+              </ul>
+
+              <div className="footer--contact">
+                <a href='mailto:stepansubrt@icloud.com' target='_blank'><AiOutlineMail /></a>
+                <a href='https://github.com/subinho' target='_blank'><AiFillGithub /></a>
+                <a href='#'><AiFillLinkedin /></a>
+              </div>
+              
             </div>
-            
-            <ul className="footer--nav">
-                <li><a href='#'>HOME</a></li>
-                <li><a href='#'>PROJECTS</a></li>
-                <li><a href='#'>ABOUT</a></li>
-                <li><a href='#'>CONTACT</a></li>
-            </ul>
-
-            <ul className="footer--contact">
-              <AiOutlineMail />
-              <AiFillGithub />
-              <AiFillLinkedin />
-            </ul>
-            
-          </div>
       </section>
   )
 }
