4268a3ff8ff8e0cd8ed299b5c15bffab4a08dee6
Stepan Subrt <stepansubrt@icloud.com>
2022-04-08 00:31:24 +0200

    change: deleted hardcoded sidebar and implemented sidebar component

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import './Introduction.css'
 import img from '../../images/intro-image.svg'
+import Sidebar from '../Sidebar/Sidebar'
 import { BsGithub, BsLinkedin, BsMessenger, BsWhatsapp} from 'react-icons/bs'
 import { AiOutlineMail } from 'react-icons/ai'
 
 const Introduction = () => {
   return (
-    <section className='introduction full_page'>
-      <div className="introduction--socials">
-        <a href='#'><BsLinkedin /></a>
-        <a href='#'><BsGithub /></a>
-        <a href='#'><AiOutlineMail /></a>
-        <a href='#'><BsMessenger /></a>
-        <a href='#'><BsWhatsapp /></a>
-      </div>
+    <section className='introduction full_page' id='home'>
+        <Sidebar />
         <div className="introduction--image">
           <img src={img}/>
         </div>
@@ -21,7 +16,7 @@ const Introduction = () => {
           <h3>Hello, I'm</h3>
           <h1>ŠTĚPÁN ŠUBRT</h1>
           <p>WEB DEVELOPER</p>
-          <button className="btn-primary"><a href='#'>PROJECTS</a></button>
+          <button className="btn-primary"><a href='#projects'>PROJECTS</a></button>
         </div>
           <div className='scroll_down'></div>
     </section>
