21248a1719f19c652afe17410b2024288ed69e0f
Stepan Subrt <stepansubrt@icloud.com>
2022-04-10 02:15:47 +0200

    build: added context for chaging theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, createContext } from 'react'
 
 import Header from './components/Header/Header'
 import Introduction from './components/Introduction/Introduction'
@@ -7,16 +7,24 @@ import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
+export const ThemeContext = createContext()
+
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false)
+  
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme)
+  }
+
   return (
-    <>
+    <ThemeContext.Provider value={{darkTheme, toggleTheme}}>
       <Header />
       <Introduction />
       <Projects />
       <About />
       <Contact />      
       <Footer />
-    </>
+    </ThemeContext.Provider>
   )
 }
 
