a5df9f45d9d134fb7dcc4388c836e6a0dd056e8c
Stepan Subrt <stepansubrt@icloud.com>
2022-06-20 02:21:48 +0200

    build: updated links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -30,11 +30,11 @@ const Footer = () => {
               </ul>
 
               <div className="footer--contact">
-                <a href='#' target='_blank'><BsLinkedin /></a>
+                <a href='https://www.linkedin.com/in/%C5%A1t%C4%9Bp%C3%A1n-%C5%A1ubrt-19b981242/' target='_blank'><BsLinkedin /></a>
                 <a href='https://github.com/subinho' target='_blank'><BsGithub /></a>
-                <a href='mailto:stepansubrt@icloud.com'><AiOutlineMail /></a>
-                <a href='#' target='_blank'><BsMessenger /></a>
-                <a href='#' target='_blank'><BsWhatsapp /></a>             
+                <a href='mailto:stepansubrt@icloud.com' target='_blank'><AiOutlineMail /></a>
+                <a href='https://m.me/stepan.subrt.52' target='_blank'><BsMessenger /></a>
+                <a href='https://api.whatsapp.com/send?phone=+420774237299' target='_blank'><BsWhatsapp /></a>             
               </div>
               
             </div>
