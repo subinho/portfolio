a0c3d0fa48128241cce5dd63f8fbac51cd2ebf93
Stepan Subrt <stepansubrt@icloud.com>
2022-06-20 02:21:15 +0200

    build: updated links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,11 +8,11 @@ const Sidebar = () => {
       <>
 
           <div className="introduction--socials">
-            <a href='#'><BsLinkedin /></a>
-            <a href='#'><BsGithub /></a>
-            <a href='#'><AiOutlineMail /></a>
-            <a href='#'><BsMessenger /></a>
-            <a href='#'><BsWhatsapp /></a>
+            <a href='https://www.linkedin.com/in/%C5%A1t%C4%9Bp%C3%A1n-%C5%A1ubrt-19b981242/' target='_blank'><BsLinkedin /></a>
+            <a href='https://github.com/subinho' target='_blank'><BsGithub /></a>
+            <a href='mailto:stepansubrt@icloud.com' target='_blank'><AiOutlineMail /></a>
+            <a href='https://m.me/stepan.subrt.52' target='_blank'><BsMessenger /></a>
+            <a href='https://api.whatsapp.com/send?phone=+420774237299' target='_blank'><BsWhatsapp /></a>
           </div>
 
       </>
